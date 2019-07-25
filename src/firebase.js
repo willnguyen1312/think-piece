1e43668713750b997a2fbd796fde1046242d4dcf
Nam Nguyen <namnguyen@axon.com>
2019-07-25 15:51:17 +0700

    Voila

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmCvOgWehZgwLqN76OtYQpfcD1QLwG6q4",
+  authDomain: "fm-live-29e10.firebaseapp.com",
+  databaseURL: "https://fm-live-29e10.firebaseio.com",
+  projectId: "fm-live-29e10",
+  storageBucket: "",
+  messagingSenderId: "529969028224",
+  appId: "1:529969028224:web:8ec351de0a937349"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+window.firebase = firebase;
+
+export const firestore = firebase.firestore();
+
+export default firebase;
